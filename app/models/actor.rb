0cd684ec598d1a8f6889a9df67352fd36db1db43
Chris Junker <elevatemedfit@gmail.com>
2019-08-29 19:29:07 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.artists.collect {|artist| artist.first_name, artist.last_name}
+    self.artists.collect {|artist| artist.first_name}
   end
 end
